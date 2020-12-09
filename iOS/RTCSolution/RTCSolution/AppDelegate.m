edb9b46b6ed5668432a443c6759f450770f9cf89
术专 <burui.br@alibaba-inc.com>
2020-12-09 15:08:39 +0800

    remove verbose code

--- iOS/RTCSolution/RTCSolution/AppDelegate.m
+++ iOS/RTCSolution/RTCSolution/AppDelegate.m
@@ -26,10 +26,6 @@
     [self.window setRootViewController:nav];
     [self.window makeKeyAndVisible];
     
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        [self checkVersion];
-    });
-    
     return YES;
 }
 
